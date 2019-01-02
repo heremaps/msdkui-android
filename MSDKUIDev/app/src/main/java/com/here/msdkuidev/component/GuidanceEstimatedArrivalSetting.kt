5ac1ad2fe511327ce1152e0141c79bb9327b8a68
Vijay Yadav <5671997+imvjk@users.noreply.github.com>
2019-01-02 14:02:59 +0100

    Added missing copyright headers
    Signed-off-by: Vijay Yadav <5671997+imvjk@users.noreply.github.com>

--- MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceEstimatedArrivalSetting.kt
+++ MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceEstimatedArrivalSetting.kt
@@ -1,15 +1,29 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.here.msdkuidev.component
 
 import android.content.Context
 import android.os.Parcel
 import android.os.Parcelable
 import com.here.msdkui.guidance.GuidanceEstimatedArrivalViewData
-import com.here.msdkuidev.Constant
 import com.here.msdkuidev.R
 import com.here.msdkuidev.Setting
 import com.here.msdkuidev.SettingItem
 import java.util.*
-import kotlin.collections.LinkedHashMap
 
 class GuidanceEstimatedArrivalSetting : Setting<GuidanceEstimatedArrival>() {
 
--- MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceNextManeuverViewSetting.kt
+++ MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceNextManeuverViewSetting.kt
@@ -1,19 +1,28 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.here.msdkuidev.component
 
 import android.content.Context
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.os.Parcel
 import android.os.Parcelable
-import com.here.msdkui.guidance.GuidanceManeuverData
+import com.here.msdkui.guidance.GuidanceNextManeuverData
 import com.here.msdkuidev.R
 import com.here.msdkuidev.Setting
 import com.here.msdkuidev.SettingItem
-import kotlin.collections.LinkedHashMap
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.support.v4.content.ContextCompat
-import com.here.msdkui.guidance.GuidanceNextManeuverData
 
 
 class GuidanceNextManeuverViewSetting() : Setting<GuidanceNextManeuverView>() {

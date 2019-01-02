9405ad5445bef07977a92ec571cbc1079a9916c2
Marcin Lawicki <41331357+m1awicki@users.noreply.github.com>
2019-01-02 16:20:37 +0100

    Add new component GuidanceStreetLabel.
    Signed-off-by: Marcin Lawicki <41331357+m1awicki@users.noreply.github.com>

---
+++ MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceStreetLabel.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.here.msdkuidev.component
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.here.msdkuidev.Constant
+import com.here.msdkuidev.R
+import kotlinx.android.synthetic.main.guidance_street_label.*
+
+class GuidanceStreetLabel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val setting = intent.getParcelableExtra(Constant.ITEM) as GuidanceStreetLabelSetting.GuidanceStreetLabelSettingItem
+        val resourceId = if(setting.subTitle == Constant.DEFAULT) R.layout.guidance_street_label else
+            R.layout.guidance_street_label_fix
+        setContentView(resourceId)
+        setting.onlyView ?: run {
+            guidanceStreetLabelView.setCurrentStreetData(setting.guidanceStreetLabelData!!)
+        }
+    }
+}
---
+++ MSDKUIDev/app/src/main/java/com/here/msdkuidev/component/GuidanceStreetLabelSetting.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.here.msdkuidev.component
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Parcel
+import android.os.Parcelable
+import com.here.msdkui.guidance.GuidanceStreetLabelData
+import com.here.msdkuidev.Setting
+import com.here.msdkuidev.SettingItem
+
+class GuidanceStreetLabelSetting : Setting<GuidanceStreetLabel>() {
+
+    class GuidanceStreetLabelSettingItem : SettingItem {
+
+        var guidanceStreetLabelData: GuidanceStreetLabelData? = null
+        var onlyView : Boolean? = null
+
+        constructor() : super()
+
+        constructor(parcel: Parcel) : super(parcel) {
+            guidanceStreetLabelData =
+                    parcel.readParcelable(GuidanceStreetLabelData::class.java.classLoader)
+            onlyView = parcel.readValue(Boolean::class.java.classLoader) as? Boolean
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            super.writeToParcel(parcel, flags)
+            parcel.writeParcelable(guidanceStreetLabelData, flags)
+            parcel.writeValue(onlyView)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<GuidanceStreetLabelSettingItem> {
+            override fun createFromParcel(parcel: Parcel): GuidanceStreetLabelSettingItem {
+                return GuidanceStreetLabelSettingItem(parcel)
+            }
+
+            override fun newArray(size: Int): Array<GuidanceStreetLabelSettingItem?> {
+                return arrayOfNulls(size)
+            }
+        }
+
+    }
+
+    override fun getClassName(): Class<GuidanceStreetLabel> {
+        return GuidanceStreetLabel::class.java
+    }
+
+    override fun getItems(context: Context): LinkedHashMap<String, SettingItem> {
+        return linkedMapOf(
+            "Default" to GuidanceStreetLabelSettingItem().apply {
+                onlyView = true
+            },
+            "With all properties set" to GuidanceStreetLabelSettingItem().apply {
+                guidanceStreetLabelData = GuidanceStreetLabelData("Street Label Text", Color.BLACK)
+            },
+            "Without background color" to GuidanceStreetLabelSettingItem().apply {
+                guidanceStreetLabelData = GuidanceStreetLabelData("Street Label Text", 0)
+            },
+            "Without street name" to GuidanceStreetLabelSettingItem().apply {
+                guidanceStreetLabelData = GuidanceStreetLabelData(null, Color.BLACK)
+            }
+        )
+    }
+}

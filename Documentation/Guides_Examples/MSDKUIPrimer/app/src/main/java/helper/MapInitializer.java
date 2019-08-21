ac63e281888ff95e3d1875fb340322efe22f4310
datasun <datasun@users.noreply.github.com>
2019-08-21 15:09:27 +0200

    MSDKUI-2090: Migrate example apps to AndroidX
    Signed-off-by: datasun <datasun@users.noreply.github.com>

--- Documentation/Guides_Examples/MSDKUIPrimer/app/src/main/java/helper/MapInitializer.java
+++ Documentation/Guides_Examples/MSDKUIPrimer/app/src/main/java/helper/MapInitializer.java
@@ -19,9 +19,9 @@ package helper;
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 
 import com.here.android.mpa.common.MapSettings;
